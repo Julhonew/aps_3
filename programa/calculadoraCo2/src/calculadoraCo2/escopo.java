cab6fc2a14428453e5f3a4ff9b1710840648897e
adonai <victor_adonai@hotmail.com>
2019-05-23 15:30:35 -0300

    Update escopo.java

--- programa/calculadoraCo2/src/calculadoraCo2/escopo.java
+++ programa/calculadoraCo2/src/calculadoraCo2/escopo.java
@@ -21,42 +21,70 @@ public class escopo {
 	);
 	x.setco2(botijao.calculo());
 
-        System.out.println("Gás Natural (Gás de Encanamento)");
-	JOptionPane.ShowInputDialog("Você utiliza Gás Natural?\n 1 - Sim \n 2 - Não");
+		
+        consumogasNatural  = new consumogasNatural(
+	       JOptionPane.ShowInputDialog("Gás Natural - Gás de Encanamento
+					   \nVocê utiliza Gás Natural?
+					   \n1 - Sim 
+					   \n2 - Não"
+	);				   
 	x.setco2(consumo.calculo());
-	consumogasNatural  = new consumogasNatural(2, true);
+	  
 
-	  consumolixo consumo = new consumolixo(
-	  JOptionPane.showInputDialog("Qual o seu consumo diário de lixo (em quilos)"),
+	 consumolixo consumo = new consumolixo(
+	          JOptionPane.showInputDialog("Qual o seu consumo diário de lixo -
+					   \n Em quilos ?"),
 	  true,);
-	  x.setco2(consumo.calculo());
-
-	  JOptionPane.ShowInputDialog("Você utiliza Carro movido á Diesel?\n 1 - Sim \n 2 - Não");
-	  x.setco2(consumo.calculo());
-	  consumoveiculoDiesel  = new consumoveiculoDiesel(2, true);
-
-	  JOptionPane.ShowInputDialog("Você utiliza Carro movido á Etanol?\n 1 - Sim \n 2 - Não");
-	  x.setco2(consumo.calculo());
-	  consumoveiculoEtanol = new consumoveiculoEtanol(2, true);
-
-	  JOptionPane.ShowInputDialog("Você utiliza Carro movido á GNV?\n 1 - Sim \n 2 - Não");
-	  x.setco2(consumo.calculo());
-	  consumoveiculoGNV  = new consumoveiculoGNV(2, true);
-
-	  JOptionPane.ShowInputDialog("Você utiliza Carro movido á Gasolina?\n 1 - Sim \n 2 - Não");
-	  x.setco2(consumo.calculo());
-	  consumoveiculoGasolina  = new consumoveiculoGasolina(2, true);
-
-	  System.out.println("Transporte Aéreo - Km de Distância (ida e volta)");
-
-	  JOptionPane.ShowInputDialog("Você viajou recentemente? \n 1 - Sim \n 2 - Não");
-	  x.setco2(consumo.calculo());
-	  consumoveiculoGasolina  = new consumoveiculoGasolina(2, true);
-
-
-
-	  System.out.println("Você deverá plantar");
-	  System.out.println(calculoArvores.plantar(0,null));
+	      x.setco2(consumo.calculo());
+    
+		
+         consumoveiculoDiesel  = new consumoveiculoDiesel(
+	       JOptionPane.ShowInputDialog("
+					   \nVocê utiliza Carro movido á Diesel??
+					   \n1 - Sim 
+					   \n2 - Não"
+	);				   
+	x.setco2(consumo.calculo());
+		
+	 consumoveiculoEtanol  = new consumoveiculoEtanol(
+	       JOptionPane.ShowInputDialog("
+					   \nVocê utiliza Carro movido á Etanol??
+					   \n1 - Sim 
+					   \n2 - Não"
+	);				   
+	x.setco2(consumo.calculo());
+		  
+		 
+	  consumoveiculoGNV  = new consumoveiculoGNV(
+	       JOptionPane.ShowInputDialog("
+					   \nVocê utiliza Carro movido á GNV??
+					   \n1 - Sim 
+					   \n2 - Não"
+	);				   
+	x.setco2(consumo.calculo());
+		  
+		  
+	   consumoveiculoGasolina  = new consumoveiculoGasolina(
+	       JOptionPane.ShowInputDialog("
+					   \nVocê utiliza Carro movido á Gasolina??
+					   \n1 - Sim 
+					   \n2 - Não"
+	);				   
+	x.setco2(consumo.calculo());  
+		  
+            
+	     consumoviagens  = new consumoviagens(
+	        JOptionPane.ShowInputDialog("
+					   \nVocê viajou recentemente?
+					   \n1 - Sim 
+					   \n2 - Não"
+	);				   
+	x.setco2(consumo.calculo());
+		 
+		     
+		     
+                calculoArvores.plantar() ;
+	  
 
 
        }
