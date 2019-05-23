a400c6e0eca729fc1988ed470dd19aa749af46d5
adonai <victor_adonai@hotmail.com>
2019-05-22 23:17:17 -0300

    Update escopo.java

--- programa/calculadoraCo2/src/calculadoraCo2/escopo.java
+++ programa/calculadoraCo2/src/calculadoraCo2/escopo.java
@@ -21,12 +21,11 @@ public class escopo {
 		  x.setco2(consumo.calculo());
 	          consumogasBorijao  = new consumogasBorijao(2, true);
 		
-		
-		  JOptionPane.showMessageDialog(null, "Gás Natural (Gás de Encanamento)")
-	          consumogasNatural consumo = new consumogasNatural(
-		  JOptionPane.showInputDialog("Qual o seu consumo mensal de gás natural em M3"),
-		  true, false);
+		  System.out.println("Gás Natural (Gás de Encanamento)");
+ 		
+		  JOptionPane.ShowInputDialog("Você utiliza Gás Natural?\n 1 - Sim \n 2 - Não");
 		  x.setco2(consumo.calculo());
+	          consumogasNatural  = new consumogasNatural(2, true);
 		
                   consumolixo consumo = new consumolixo(
 		  JOptionPane.showInputDialog("Qual o seu consumo diário de lixo (em quilos)"),
@@ -37,10 +36,34 @@ public class escopo {
 		  x.setco2(consumo.calculo());
 	          consumoveiculoDiesel  = new consumoveiculoDiesel(2, true);
 		
+		  JOptionPane.ShowInputDialog("Você utiliza Carro movido á Etanol?\n 1 - Sim \n 2 - Não");
+		  x.setco2(consumo.calculo());
+	          consumoveiculoEtanol = new consumoveiculoEtanol(2, true);
+		
+		  JOptionPane.ShowInputDialog("Você utiliza Carro movido á GNV?\n 1 - Sim \n 2 - Não");
+		  x.setco2(consumo.calculo());
+	          consumoveiculoGNV  = new consumoveiculoGNV(2, true);
+		  
+		  JOptionPane.ShowInputDialog("Você utiliza Carro movido á Gasolina?\n 1 - Sim \n 2 - Não");
+		  x.setco2(consumo.calculo());
+	          consumoveiculoGasolina  = new consumoveiculoGasolina(2, true);
+		
+		  System.out.println("Transporte Aéreo - Km de Distância (ida e volta)");
+		
+		  JOptionPane.ShowInputDialog("Você viajou recentemente? \n 1 - Sim \n 2 - Não");
+		  x.setco2(consumo.calculo());
+	          consumoveiculoGasolina  = new consumoveiculoGasolina(2, true);
+		
+		 
+		  
+		  System.out.println("Você deverá plantar");
+		  System.out.println(calculoArvores.plantar(0,null));
+		  
+		
                }
 		
 		
-	
+	       
 		
 		
 		
